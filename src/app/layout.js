3abb1d23eee5d278096d808ec2a94846f39cb628
thedeepakmaurya <deipakmaurya@gmail.com>
2024-12-24 15:24:09 +0530

    meta tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Sidenav from "@/components/SideNav";
 import { Montserrat } from "next/font/google";
 // In _app.js or in your component
 import 'boxicons/css/boxicons.min.css';
+import Head from "next/head";
 
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -17,6 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="AKJzXLR56cBvKAyymdgZB1914DXZvxOj7WgGuDyqINw" />
+      </Head>
       <body
         className={montserrat?.className}
       >
