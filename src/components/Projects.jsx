55cdf89594833a91d6ee1e5f595c837295e8ad67
thedeepakmaurya <deipakmaurya@gmail.com>
2025-01-26 22:49:47 +0530

    layout changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,23 +11,23 @@ const Projects = () => {
       <div className="lg:w-[70%] xl:w-[60%] w-full h-auto flex flex-col gap-6 bg-secondary rounded-md px-4 py-10 shadow-lg">
         {projects.map((project, index) => (
           <Link href={project.link} key={index} target="_blank">
-            <div className="w-auto h-auto flex items-center justify-end gap-3 xl:px-4 px-2">
-              <div>
-                <h1 className="text-right">{project.title}</h1>
-                <h2 className="text-sm text-right">{project.description}</h2>
-                <div className="flex justify-end gap-2">
-                  <i className="bx bxl-javascript bx-xs"></i>
-                  <i className="bx bxl-react bx-xs"></i>
-                  <i className="bx bxl-tailwind-css bx-xs"></i>
-                </div>
-              </div>
+            <div className="w-auto h-auto flex items-center gap-3 xl:px-4 px-2">
               <Image
-                className="rounded-full w-10 h-10 border-2 border-foreground shadow-lg object-cover "
+                className="rounded-full w-9 h-9 border-2 border-foreground shadow-lg object-cover "
                 src={project.image}
                 alt="project image"
                 width={100}
                 height={100}
               />
+              <div>
+                <h1 className="">{project.title}</h1>
+                <h2 className="text-sm ">{project.description}</h2>
+                <div className="flex gap-2">
+                  <i className="bx bxl-javascript bx-xs"></i>
+                  <i className="bx bxl-react bx-xs"></i>
+                  <i className="bx bxl-tailwind-css bx-xs"></i>
+                </div>
+              </div>
             </div>
           </Link>
         ))}
